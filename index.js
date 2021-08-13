d9560710d09555e1d46b7e6946d8d4082717aa12
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-08-13 22:19:12 +0100

    master

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 
 const slides = document.getElementsByClassName("carousel-item");
 let slidePosition = 0;
-const totalSlides = slides.length
+const totalSlides = slides.length;
 
 let btnPrev = document.getElementById('carousel-button-prev')
 let btnNext = document.getElementById('carousel-button-next')
@@ -19,20 +19,30 @@ let btnNext = document.getElementById('carousel-button-next')
 btnPrev.addEventListener("click", moveToPrevSlide);
 btnNext.addEventListener("click", moveToNextSlide);
 
-function moveToPrevSlide(){
-
+function hideAllSlides(){
+    for (let slide of slides){
+        slide.classList.remove("carousel-item-visible");
+        slide.classList.add("carousel-item-hidden");
+        
+    }   
 }
 
 function moveToNextSlide(){
-   
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-        } else {
-            slidePosition++;
-        }
-        slides[slidePosition].classList.add('carousel-item-visible')
-
+    hideAllSlides();
     
+    if(slidePosition === totalSlides - 1){
+      slidePosition = 0;
+  } else {
+      slidePosition++;
+      
+  }
+  slides[slidePosition].classList.add("carousel-item-visible");
+  
+  
 }
 
 
+
+function moveToPrevSlide(){
+
+}
