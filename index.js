356766b231363a4bf2ca673b412b5f69d28f7be4
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-08-14 23:14:52 +0100

    ´

--- index.js
+++ index.js
@@ -42,8 +42,12 @@ function moveToNextSlide(){
   
 }
 
-
-
 function moveToPrevSlide(){
-
+    hideAllSlides();
+    if(slidePosition === 0){
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
 }
